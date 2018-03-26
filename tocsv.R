06be12d4be98fd4edc4e701b673759b0098d267e
Jack <jacklukemcmurray@gmail.com>
2018-03-26 16:51:45 +0100

    Fixed spelling discrepancies

--- tocsv.R
+++ tocsv.R
@@ -25,18 +25,25 @@ subTdir1 <- c("/Volumes/ResearchData/Vectra/Vectra3/Fedor/nahla/Exports/Cell_seg
 
 preprocess_cell_seg_summary(folderoffolders, targetdir1, subTdir1, origindir)
 
-CD1 <- factorthese
+combined_summary_df <- read.csv("Data/combined_summary_df.csv")
 
-Number_of_Images <- data.frame(Slide = character(),
-                     Number_of_Fields = double(),
-                     stringsAsFactors = F)
-c <- 1
-for(i in levels(CD1$Slide.ID)){
-  print(i)
-  work <- droplevels(subset(CD1, Slide.ID == i))
-  nom <- nlevels(work$Sample.Name)
-  Number_of_Images[c, "Slide"] <- i
-  Number_of_Images[c, "Number_of_Fields"] <- nom
-  c <- c + 1
-}
-writeCsvO(Number_of_Images)
+# # Number of images per slide
+# CD1 <- factorthese(combined_summary_df, c("Sample.Name", "Tissue.Category", "Slide.ID", "Phenotype"))
+# 
+# Number_of_Images <- data.frame(Slide = character(),
+#                      Number_of_Fields = double(),
+#                      stringsAsFactors = F)
+# c <- 1
+# for(i in levels(CD1$Slide.ID)){
+#   print(i)
+#   work <- droplevels(subset(CD1, Slide.ID == i))
+#   nom <- nlevels(work$Sample.Name)
+#   Number_of_Images[c, "Slide"] <- i
+#   Number_of_Images[c, "Number_of_Fields"] <- nom
+#   c <- c + 1
+# }
+# writeCsvO(Number_of_Images)
+
+# Fix discrepancies in spelling
+combined_summary_df$Tissue.Category <- fix_tissue_cat(combined_summary_df)
+combined_summary_df$Phenotype <- fix_pheno(combined_summary_df)
