265f75a046edb5a00c64570461b37429e8b064d4
Jack <jacklukemcmurray@gmail.com>
2018-03-26 14:49:06 +0100

    Should work...

--- tocsv.R
+++ tocsv.R
@@ -24,3 +24,19 @@ targetdir1 <- c("/Volumes/ResearchData/Vectra/Vectra3/Fedor/nahla/Exports/Cell_s
 subTdir1 <- c("/Volumes/ResearchData/Vectra/Vectra3/Fedor/nahla/Exports/Cell_seg_summary_only/CSV")
 
 preprocess_cell_seg_summary(folderoffolders, targetdir1, subTdir1, origindir)
+
+CD1 <- factorthese
+
+Number_of_Images <- data.frame(Slide = character(),
+                     Number_of_Fields = double(),
+                     stringsAsFactors = F)
+c <- 1
+for(i in levels(CD1$Slide.ID)){
+  print(i)
+  work <- droplevels(subset(CD1, Slide.ID == i))
+  nom <- nlevels(work$Sample.Name)
+  Number_of_Images[c, "Slide"] <- i
+  Number_of_Images[c, "Number_of_Fields"] <- nom
+  c <- c + 1
+}
+writeCsvO(Number_of_Images)
